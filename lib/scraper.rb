6596f76737cd86d9b181215c72334d6a6ee16b25
David Acosta <davidacosta25081@yahoo.com>
2019-02-28 06:36:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,16 +16,7 @@ end
 
 
 
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+    
 
 end
 
